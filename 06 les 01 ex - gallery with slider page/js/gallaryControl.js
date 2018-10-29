03d1664857d694f35318102fcfe1fb5aa79626fa
Alex-Smil <alexsmile39@gmail.com>
2018-10-29 03:47:25 +0300

    авто замена Image идет с текущей картинки

--- 06 les 01 ex - gallery with slider page/js/gallaryControl.js
+++ 06 les 01 ex - gallery with slider page/js/gallaryControl.js
@@ -1,7 +1,8 @@
 
 // ******** Глобальные переменные **********
-let indexImg = 0;
+let globIndexImg = 1;
 let delay = 2000;
+let loopInterval;
 
 
 
@@ -14,41 +15,60 @@ function init() {
     let galleryImages = document.getElementsByClassName("b-gallery__image"); 
 
     // *** вешаем на каждую картинку addEventListener();
-    for(let i = 0 ; i < galleryImages.length; i++) {
-        galleryImages[i].addEventListener("click", changeBigPicture);
+    for(let index = 0 ; index < galleryImages.length; index++) {
+        galleryImages[index].addEventListener("click", function() {
+            changeBigPicture(galleryImages[index], index);
+        });
     }
 
+    // loopInterval = setInterval(avtoChangeBigPicture, delay, galleryImages);
     setInterval(avtoChangeBigPicture, delay, galleryImages);
+
 } 
 
-function changeBigPicture(eventObj) {
+// change image in loop
+// создает псевдо объект для avtoChangeBigPicture и вызывает её
+function avtoChangeBigPicture(galleryImages) {
+    // псевдообъект для 
+    // задаем литералом
+    // let psevdoImg = galleryImages[globIndexImg];
+    changeBigPicture(galleryImages[globIndexImg], globIndexImg);
+    globIndexImg++;
+    if(globIndexImg >= galleryImages.length) {
+        globIndexImg = 0;
+    }
+}
+
+function changeBigPicture(eventObj, index) {
+
     // *** извлекаем bigImage для дальнейшей подмены её src
     let bigImage = document.querySelector(".b-gallery__bigImage");
 
     // *** воспользуемся фитчей js, при наступлении события в changeBigPicture по умолчанию
     // передается неявный аргумент eventObj (назвать можно как угодно) в котором хранится ссылка на объект вызвавший это событие
     // извлекаем сам объект вызвавший событие через св-во target объекта eventObj
-    let smallImage = eventObj.target;
+    // let smallImage = eventObj.target;
     // console.log("eventObj.target" + eventObj.target);
 
     // *** извлекаем нужные данные из него, нам нужен src
-    let src = smallImage.src;
+    // let src = smallImage.src;
+    let src = eventObj.src;
 
     // **********  ex 1 tester ***********
     // ;-) blue fatalitist
-    // src = undefined;
+    // src = undefined; 
     // ***********************************
-
-    if(isExistImage(src)) {
-        // *** подставляем полученный src в значение аттрибута src="" из bigImage
-        // изменения незамедлительно отобразаться в DOM на страничке
-        bigImage.src = src;
-        // indexImg = 
-    } else {
+    if(!isExistImage(src)) {
         // если картинки нет выводим предупреждение
         // шутками-шутками, а модальное окно или вставку спец Img в этом случае доделать.
         blueFatality();
     }
+    
+    // *** подставляем полученный src в значение аттрибута src="" из bigImage
+    // изменения незамедлительно отобразаться в DOM на страничке
+    bigImage.src = src;
+    // indexImg = // узнат под каким индексом эта картинка в galleryImages !!!!
+    globIndexImg = index;
 }
 
 // ex 1 tester
@@ -73,20 +93,7 @@ function blueFatality() {
     body.insertBefore(image, firstBlock);
 }
 
-// change image in loop
-// создает псевдо объект для avtoChangeBigPicture и вызывает её
-function avtoChangeBigPicture(galleryImages) {
-    // псевдообъект для 
-    // задаем литералом
-    let psevdoImg = {
-        target: galleryImages[indexImg]
-    };
-    changeBigPicture(psevdoImg);
-    indexImg++;
-    if(indexImg >= galleryImages.length) {
-        indexImg = 0;
-    }
-}
+
 
 
 
